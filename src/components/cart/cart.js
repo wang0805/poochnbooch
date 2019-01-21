2470f632beb150691f6ab6ce8a99d25a4df77dea
Wenhao Wang <wh_wang@live.com>
2019-01-21 17:30:49 +0800

    chores: fix context

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -16,30 +16,12 @@ class Cart extends Component {
     }
   }
 
-  addToCart(newItem) {
-    let itemExisted = false;
-    let updatedCart = this.context.cart.map(item => {
-      if (newItem === item.sku) {
-        itemExisted = true;
-        return { sku: item.sku, quantity: ++item.quantity };
-      } else {
-        return item;
-      }
-    });
-    if (!itemExisted) {
-      updatedCart = [...updatedCart, { sku: newItem, quantity: 1 }];
-    }
-    this.context.setCart(updatedCart);
-    // Store the cart in the localStorage.
-    localStorage.setItem('stripe_checkout_items', JSON.stringify(updatedCart));
-  }
-
   render() {
     return (
       <div>
         <Checkout cart={this.context.cart} />
         {React.cloneElement(this.props.children, {
-          addToCart: this.addToCart.bind(this),
+          addToCart: this.context.addToCart,
           cart: this.context.cart,
         })}
       </div>
